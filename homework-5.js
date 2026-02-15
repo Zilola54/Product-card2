5ac5224d3ac40f0db5a1c3ee62fb20f0810d4b93
Zilola54 <olimova03_95@mail.ru>
2026-02-15 13:45:52 +0700

    (Homework № 5): change names functions

--- homework-5.js
+++ homework-5.js
@@ -1,47 +1,46 @@
 
 //Функция, показывающая город и температуру
-function weather(Y,X) {
-  console.log(`Сейчас в ${Y} температура  — ${X} градусов по Цельсию`)
+function weather(city, temperature) {
+  console.log(`Сейчас в ${city} температура  — ${temperature} градусов по Цельсию`)
 }
 weather('Екатеринбурге', -25)
 
 //Функция, опеределяющая скорость света
-let lightSpeed = 299792458;
+let LIGHT_SPEED = 299792458;
 
-if (lightSpeed > 299792458) {
+if (LIGHT_SPEED > 299792458) {
   console.log("superLuminal");
-} else if (lightSpeed === 299792458) {
+} else if (LIGHT_SPEED === 299792458) {
   console.log("lightSpeed");
-} else (lightSpeed < 299792458); {
+} else (LIGHT_SPEED < 299792458); {
   console.log("subLuminal");
 }
-console.log(lightSpeed)
+console.log(LIGHT_SPEED)
 
 //Функция, помогающая подсчитать нужное кол-во денег для покупки яблок
-let product = "яблоко";
-let productPrice = 6;
-let myMoney = 6;
+function buyProduct(product, price, budget) {
+  let difference = price - budget;
 
-let difference = productPrice - myMoney;
-
-if (myMoney >= productPrice) {
-  console.log(`${product} приобретёно. Спасибо за покупку!`);
+  if (budget >= price) {
+    console.log(`${product} приобретёно. Спасибо за покупку!`);
 } else {
-  console.log(`Вам не хватает ${difference}$, пополните баланс`);
+    console.log(`Вам не хватает ${difference}$, пополните баланс`);
+  }
 }
+buyProduct ("Груша", 8, 5)
 
 //Доп.задания
-function buyHouse (house, myMone) {
+function buyProperty (property , myBudget) {
 
-  if (myMone <= house) {
-    let diff = house - myMone;
+  if (myBudget <= property) {
+    let diff = property - myBudget;
     console.log(`не хватает ${diff}`)
-  } else if (myMone >= house) {
-    let change = myMone - house;
+  } else if (myBudget >= property) {
+    let change = myBudget - property;
     console.log(`при покупке дома останется ${change}`);
   }
 }
-buyHouse(1000, 500)
+buyProperty(1000, 5000)
 
 let city = "Moscow";
 const myAge = 30;
