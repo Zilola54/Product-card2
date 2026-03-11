0097e26e29fa09382d2bd58a8c16968b390574fb
Zilola54 <olimova03_95@mail.ru>
2026-03-12 00:19:24 +0700

    (Homework №7) creating arrays using methods

--- homework-7.js
+++ homework-7.js
@@ -1,84 +1,73 @@
+import { socialComments } from "./comments.js";
+
 //уровень 1 задание 2
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const newNumbers = numbers.slice(4,10);
+const filteredNumbers = numbers.slice(4, 10);
 
-console.log(newNumbers);
+console.log(filteredNumbers);
 
 //задание 3
 
-const pottedPlants = ["Хлорофитум", "Сансевиерия", "Алоэ вера", "Замиокулькас", "Эпипремнум", "Фикус Бенджамина"];
+const plants = ["Хлорофитум", "Сансевиерия", "Алоэ вера", "Замиокулькас", "Эпипремнум", "Фикус Бенджамина"];
 
-const pottedPlantsNew = pottedPlants.includes("Алоэ вера");
-
-console.log(pottedPlantsNew);
+console.log(plants.includes("Алоэ вера"));
 
 //задание 4
+function getReversed(array) {
+  return array.reverse();
+};
+console.log(getReversed(plants));
 
-pottedPlants.reverse();
-
-console.log(pottedPlants);
-
-numbers.reverse();
-
-console.log(numbers);
+console.log(getReversed(numbers));
 
 //уровень 2 
-//задание 6
-
-import { socialComments } from "./comments.js";
-
 //задание 7
 
-const hasCommentsCom = socialComments.filter(comment => comment.email.endsWith(".com"));
+const commentsWithComEmails = socialComments.filter(comment => comment.email.endsWith(".com"));
 
-console.log(hasCommentsCom);
+console.log(commentsWithComEmails);
 
 //задание 8
 
-const iterateId = socialComments.map(comment => ({...comment, posId: comment.id <= 5 ? 2 : 1 }));
-console.log(iterateId)
+const updatedComments = socialComments.map(comment => ({...comment, postId: comment.id <= 5 ? 2 : 1 }));
+console.log(updatedComments)
 
 //задание 9
 
-const getNewComments = socialComments.map(comment => ({
+const formattedComments = socialComments.map(comment => ({
   id: comment.id,
   name: comment.name
 }));
-console.log(getNewComments)
+console.log(formattedComments)
 
 //задание 10
 
+const commentsWithValidation = socialComments.map(comment => ({...comment, isInvalid: comment.body.length > 180}));
 
-
-const updateSocialComments = socialComments.map(comment => ({...comment, isInvalid: comment.body.length > 180}));
-
-console.log(updateSocialComments)
-
-//задание 11
-
-const totalComments = socialComments.reduce((total, comment) => total + comment.body.length,0);
-console.log(totalComments);
+console.log(commentsWithValidation)
 
 //уровень 3 
 //задание 11
 
-const onlyEmails = socialComments.reduce((acc, comment) => {acc.push({ email: comment.email});
-return acc;}, []);
+const emailsByReduce = socialComments.reduce((acc, comment) => {
+  acc.push(comment.email);
+  return acc;
+}, []);
 
-console.log(onlyEmails);
+console.log(emailsByReduce);
 
-const getEmails = socialComments.map(comment => ({ email: comment.email}));
+const emailsByMap = socialComments.map(comment => (comment.email));
 
-console.log(getEmails);
+console.log(emailsByMap);
 
 //задание 12
 
-const lineComment = onlyEmails.map(f => f.email).toString();
+const emailsByString = emailsByMap.toString();
 
-console.log(lineComment);
+console.log(emailsByString);
 
-const beautifulLineEmails = onlyEmails.map(f => f.email).join("-//---//-");
+const bemailsByJoin = emailsByMap.join("  -  ");
 
-console.log(beautifulLineEmails); 
+console.log(bemailsByJoin); 
