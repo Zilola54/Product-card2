c1d955ee28b24f3d6e4f23f2e35114534ab0e7b7
Zilola54 <olimova03_95@mail.ru>
2026-04-07 08:00:53 +0700

    Homework №9: form and modal window

---
+++ homework-9.js
@@ -0,0 +1,110 @@
+import { productCards } from "./product-cards.js";
+
+//задание 3
+
+const productsList = document.querySelector('.products-list');
+const productTemplate = document.querySelector('#product-template');
+
+function renderCards(cardsArray) {
+
+  productsList.replaceChildren();
+
+  cardsArray.forEach(card => {
+    const cardClone = productTemplate.content.cloneNode(true);
+
+    cardClone.querySelector('.product-title').textContent = card.title;
+    cardClone.querySelector('.product-image').src = `./image/${card.image}.png`;
+    cardClone.querySelector('.product-skin-type').textContent = card.skinType;
+    cardClone.querySelector('.product-description').textContent = card.description;
+    cardClone.querySelector('.price').textContent = `${card.price} ₽`;
+    
+    const listContainer = cardClone.querySelector('.product-compound');
+    listContainer.textContent = ''; 
+    
+    card.compound.forEach(text => {
+      const li = document.createElement('li');
+      li.textContent = text;
+      listContainer.append(li);
+    });
+
+    productsList.append(cardClone);
+  });
+}
+
+function getCardsCount() {
+    return 5;
+}
+
+const count = getCardsCount();
+if (count > 0) {
+  renderCards(productCards.slice(0, count));
+
+}
+
+//задание 4
+const subscribeForm = document.querySelector('#subscribe-form')
+subscribeForm.addEventListener('submit',(event) => {
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form)
+  const data = Object.fromEntries(formData.entries());
+  console.log(data);
+})
+
+//уровень 2 задание 6 создано модальное окно
+
+const overlay = document.querySelector('.overlay');
+const btnOpen = document.querySelector('.open-btn');
+
+const cross = document.querySelector('.close-cross');
+const btnCancel = document.querySelector('#cancel-btn')
+
+const closeModal = () => {
+  overlay.classList.remove('modal-showed');
+};
+
+btnOpen.onclick = () => {
+  overlay.classList.add('modal-showed');
+};
+cross.onclick = closeModal;
+btnCancel.onclick = closeModal;
+
+overlay.onclick = (e) => {
+  if(e.target === overlay) closeModal();
+};
+
+//Добавлена дата создания 
+
+let user;
+const regForm = document.querySelector('#reg-form');
+regForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(regForm);
+  const values = Object.fromEntries(formData.entries());
+
+  if (values.password !== values.confirmPassword) {
+    alert('Пароли не совпадают! Попробуйте еще раз.');
+    return;
+  }
+
+  values.password = '*'.repeat(values.password.length);
+  delete values.confirmPassword;
+
+values.createdOn = new Date();
+
+console.log('ПРОВЕРКА: Дата создана?', values.createdOn); 
+console.log('ИТОГОВЫЙ ОБЪЕКТ:',values);
+
+  console.log('Данные проверены и готовы:', values);
+  alert('Регистрация прошла успешно!');
+  closeModal();
+
+  user = values;
+
+console.log(user);
+});
+
+
+
+
