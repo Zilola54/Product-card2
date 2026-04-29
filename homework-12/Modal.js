47c148e7bc762ec3b99f386568f5a0ade4d4aff1
Zilola54 <olimova03_95@mail.ru>
2026-04-30 05:00:45 +0700

    (Homework №12):  fixed bag

--- homework-12/Modal.js
+++ homework-12/Modal.js
@@ -2,42 +2,43 @@ export class Modal {
   constructor(modalId, buttonId, shouldCloseOnOverlay) {
     this.modal = document.getElementById(modalId);
     this.overlay = document.getElementById('overlay');
+    this.shouldCloseOnOverlay = shouldCloseOnOverlay;
     this.#initOpen(buttonId);
-    this.#initClose(shouldCloseOnOverlay);
   }
 
-  open() {
+  handleOverlayClick = (event) => {
+    if (event.target === this.overlay) {
+      this.close();
+    }
+  }
+
+  open = () => {
     this.modal.classList.add('modal-showed');
     this.overlay.classList.add('overlay-showed');
+
+    if (this.shouldCloseOnOverlay) {
+      this.overlay.removeEventListener('click', this.handleOverlayClick);
+      this.overlay.addEventListener('click', this.handleOverlayClick);
+    }
+
+    const closeBtn = this.modal.querySelector('.modal-close-button');
+    if (closeBtn) {
+      closeBtn.onclick = this.close;
+    }
   }
 
-  close() {
+  close = () => {
     this.modal.classList.remove('modal-showed');
     this.overlay.classList.remove('overlay-showed');
-  }
-
-  isOpen() {
-    return this.modal.classList.contains('modal-showed');
+    this.overlay.removeEventListener('click', this.handleOverlayClick);
   }
 
   #initOpen(buttonId) {
     const button = document.getElementById(buttonId);
-    button.addEventListener('click', () => {
-      this.open();
-    })
-  }
-
-  #initClose(shouldCloseOnOverlay) {
-    const closeButton = this.modal.querySelector('#modal-close-button')
-
-    closeButton.addEventListener('click', () => {
-      this.close();
-    })
-
-    this.overlay.addEventListener('click', (e) => {
-      if (e.target === this.overlay) {
-        this.close();
-      }
-    });
+    if (button) {
+      button.addEventListener('click', () => this.open());
+    } else {
+      console.error(`Кнопка с id "${buttonId}" не найдена!`);
+    }
   }
 }
--- homework-12/script.js
+++ homework-12/script.js
@@ -1,5 +1,5 @@
 import { Modal } from "./Modal.js";
 
 const registrationModal = new Modal('registration-modal', 'registration-button', true);
-const authModal = new Modal('auth-modal', 'auth-button', false)
+const authModal = new Modal('auth-modal', 'auth-button', true)
 
