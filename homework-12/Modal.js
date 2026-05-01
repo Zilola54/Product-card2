08443d9a84280e9a0e899ec3917d037efa8c4263
Zilola54 <olimova03_95@mail.ru>
2026-05-01 14:51:43 +0700

    (Homework №12): addressed feedback

--- homework-12/Modal.js
+++ homework-12/Modal.js
@@ -4,12 +4,7 @@ export class Modal {
     this.overlay = document.getElementById('overlay');
     this.shouldCloseOnOverlay = shouldCloseOnOverlay;
     this.#initOpen(buttonId);
-  }
-
-  handleOverlayClick = (event) => {
-    if (event.target === this.overlay) {
-      this.close();
-    }
+    this.initClose(shouldCloseOnOverlay);
   }
 
   open = () => {
@@ -17,28 +12,27 @@ export class Modal {
     this.overlay.classList.add('overlay-showed');
 
     if (this.shouldCloseOnOverlay) {
-      this.overlay.removeEventListener('click', this.handleOverlayClick);
-      this.overlay.addEventListener('click', this.handleOverlayClick);
-    }
-
-    const closeBtn = this.modal.querySelector('.modal-close-button');
-    if (closeBtn) {
-      closeBtn.onclick = this.close;
+      this.overlay.addEventListener('click', this.close);
     }
   }
 
   close = () => {
     this.modal.classList.remove('modal-showed');
     this.overlay.classList.remove('overlay-showed');
-    this.overlay.removeEventListener('click', this.handleOverlayClick);
+    this.overlay.removeEventListener('click', this.close);
   }
 
   #initOpen(buttonId) {
     const button = document.getElementById(buttonId);
     if (button) {
-      button.addEventListener('click', () => this.open());
-    } else {
-      console.error(`Кнопка с id "${buttonId}" не найдена!`);
+      button.addEventListener('click', this.open);
+    } 
+  }
+
+  initClose() {
+    const closeButton = this.modal.querySelector('.modal-close-button')
+    if (closeButton) {
+      closeButton.addEventListener('click', this.close);
     }
   }
 }
